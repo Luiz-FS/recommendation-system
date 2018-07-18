78ac278237092dca9e220309b2dfd15b9952ece6
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2018-07-18 20:41:00 -0300

    Get top_5 best films

--- script.py
+++ script.py
@@ -17,8 +17,6 @@ sim_options = {'name': 'cosine',
 algo = KNNBasic(sim_options=sim_options)
 
 algo.fit(train)
-import pdb
-pdb.set_trace()
 
 uid = '196'
 iid = '242'
@@ -26,3 +24,34 @@ iid = '242'
 pred = algo.predict(uid=uid, iid=iid, r_ui=0, verbose=True)
 
 print(pred)
+
+
+# path to dataset folder
+files_dir = os.path.expanduser('~/.surprise_data/ml-100k/ml-100k/')
+
+# This time, we'll use the built-in reader.
+reader = Reader('ml-100k')
+
+# folds_files is a list of tuples containing file paths:
+# [(u1.base, u1.test), (u2.base, u2.test), ... (u5.base, u5.test)]
+train_file = files_dir + 'u%d.base'
+test_file = files_dir + 'u%d.test'
+folds_files = [(train_file % i, test_file % i) for i in [1]]
+
+data = Dataset.load_from_folds(folds_files, reader=reader)
+pkf = PredefinedKFold()
+
+sim_options = {'name': 'cosine',
+               'user_based': True  # compute  similarities between items
+               }
+
+algo = KNNBasic(sim_options=sim_options)
+
+for trainset, testset in pkf.split(data):
+
+    # train and test algorithm.
+    algo.fit(trainset)
+    predictions = algo.test(testset)
+
+    # Compute and print Root Mean Squared Error
+    accuracy.rmse(predictions, verbose=True)
