d57871171cca409e49a8743c6726783577144cca
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2018-07-19 16:33:19 -0300

    Change frontend

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,30 +1,28 @@
+'use strict';
+
 function request(url) {
     return fetch(url).then(response => response.json());
 }
 
-let form = document.getElementById('form');
-
 function get_results(event) {
-    let results = document.getElementById('results');
+    let form = document.getElementById('form');
+    let movies = document.getElementById('movies');
 
     request('/api/results' + `?uid=${form.user.value}`).then(data => {
         let li = "";
         li = data.result.reduce((movies, movie) => {
-            return movies + `<li>${movie}</li>`
+            return movies + `<li>${movie}</li>`;
         }, li);
-        results.innerHTML = li;
+        movies.innerHTML = li;
     });
     return false;
 }
 
-//form.onsubmit = get_results;
-
-let usersSelect = document.getElementById('users');
-
 request('/api/users').then(data => {
-    options = usersSelect.innerHTML;
+    let usersSelect = document.getElementById('users');
+    let options = usersSelect.innerHTML;
     options = data.users_id.reduce((options, userId) => {
-        return options + `<option value=${userId}>${userId}</option>`
+        return options + `<option value=${userId}>${userId}</option>`;
     }, options);
 
     usersSelect.innerHTML = options;
